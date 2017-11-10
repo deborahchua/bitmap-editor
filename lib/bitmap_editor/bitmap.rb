fd44f063b4888c60e35e031d0cf42716c39f6881
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2017-11-10 15:11:42 +0000

    Make basic tests pass

--- lib/bitmap_editor/bitmap.rb
+++ lib/bitmap_editor/bitmap.rb
@@ -1,3 +1,24 @@
 class Bitmap
+  def initialize (x, y)
+    @x = x
+    @y = y
+    create_bitmap
+  end
 
+  def get(x, y)
+    @bitmap[y][x]
+  end
+
+  def set(x, y, c)
+    @bitmap[y][x] = c
+  end
+
+  def clear!
+    create_bitmap
+  end
+
+  private
+  def create_bitmap
+    @bitmap = Array.new(@y){Array.new(@x, "O")}
+  end
 end
